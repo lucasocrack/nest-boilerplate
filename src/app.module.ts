656791c8c83239024121dca8ef1f2647ded00696
Lucas Campos <bhlucascampos@gmail.com>
2024-06-23 21:22:41 -0300

    criação da rota de autenticação

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './routes/user/user.module';
+import { AuthModule } from './routes/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UserModule],
+  imports: [PrismaModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
