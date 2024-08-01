c8f3db5b8c2de72608741daadac159ac4dae17d1
Lucas Campos <bhlucascampos@gmail.com>
2024-07-31 23:49:54 -0300

    entity post

--- src/routes/post/entities/post.entity.ts
+++ src/routes/post/entities/post.entity.ts
@@ -2,8 +2,13 @@ export class Post {
   id: number;
   title: string;
   content: string;
+  published: boolean;
+  date: Date;
+  price: number;
+  productDescription: string;
+  productCategory: string;
+  linkAffiliate: string;
   authorId: number;
-  createdAt: Date;
-  updatedAt: Date;
-  deletedAt: Date;
+  tags: string[];
+  category: string;
 }
