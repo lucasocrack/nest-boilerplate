209ca38a2c1484489f01ce0607fc8dd74e7e2a9b
Lucas Campos <bhlucascampos@gmail.com>
2024-08-06 07:37:35 -0300

    implemented email sending

--- src/services/email/email.module.ts
+++ src/services/email/email.module.ts
@@ -1,21 +1,46 @@
 import { Module } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
+import * as hbs from 'nodemailer-express-handlebars';
+import { join } from 'path';
+import { EmailService } from './email.service';
+import { EmailController } from './email.controller';
 
 @Module({
+  imports: [],
+  controllers: [EmailController],
   providers: [
     {
       provide: 'MAILER',
-      useValue: nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: parseInt(process.env.EMAIL_PORT, 10),
-        secure: process.env.EMAIL_SECURE === 'true',
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
-        },
-      }),
+      useFactory: () => {
+        const transporter = nodemailer.createTransport({
+          host: process.env.EMAIL_HOST,
+          port: parseInt(process.env.EMAIL_PORT, 10),
+          secure: process.env.EMAIL_SECURE === 'true',
+          auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+          },
+        });
+
+        transporter.use(
+          'compile',
+          hbs({
+            viewEngine: {
+              extname: '.hbs',
+              layoutsDir: join(__dirname, 'templates'),
+              defaultLayout: false,
+              partialsDir: join(__dirname, 'templates'),
+            },
+            viewPath: join(__dirname, 'templates'),
+            extName: '.hbs',
+          }),
+        );
+
+        return transporter;
+      },
     },
+    EmailService,
   ],
-  exports: ['MAILER'],
+  exports: [EmailService],
 })
 export class EmailModule {}
--- src/services/email/email.service.ts
+++ src/services/email/email.service.ts
@@ -1,5 +1,8 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { Transporter } from 'nodemailer';
+import * as handlebars from 'handlebars';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class EmailService {
@@ -8,14 +11,24 @@ export class EmailService {
   async sendMail(
     to: string,
     subject: string,
-    text: string,
-    html?: string,
+    template: string,
+    context: any,
   ): Promise<void> {
+    // Caminho do arquivo de template na raiz do projeto
+    const templatePath = path.join(
+      process.cwd(),
+      'src',
+      'templates',
+      `${template}.hbs`,
+    );
+    const templateString = fs.readFileSync(templatePath, 'utf-8');
+    const compiledTemplate = handlebars.compile(templateString);
+    const html = compiledTemplate(context);
+
     await this.mailer.sendMail({
       from: process.env.EMAIL_USER,
       to,
       subject,
-      text,
       html,
     });
   }
