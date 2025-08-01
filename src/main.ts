f7d53ce5e745509b7d1bd2108052e91a82d701a2
Lucas Campos <31932085+lucascampos42@users.noreply.github.com>
2025-07-31 22:31:56 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3099);
+  const port = process.env.PORT ?? 3099;
+  await app.listen(port);
+  console.log(`API rodando em: http://localhost:${port}/`);
 }
 bootstrap();
