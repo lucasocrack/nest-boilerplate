44fe0b5326b62ba48e2080ed6c05ab938d8f6fe6
Lucas Campos <31932085+lucascampos42@users.noreply.github.com>
2025-08-01 01:00:15 -0300

    remove email

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
