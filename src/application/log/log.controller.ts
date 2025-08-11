ed02b64a9a790be83501ed1a4f9e36826f3e532a
Your Name <you@example.com>
2025-08-11 01:33:10 -0300

    fix: remover JwtAuthGuard do LogController para simplificar a proteção de rotas

--- src/application/log/log.controller.ts
+++ src/application/log/log.controller.ts
@@ -1,12 +1,11 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { LogService } from './log.service';
-import { JwtAuthGuard } from '../../core/guards/jwt-auth.guard';
 import { RolesGuard } from '../../core/guards/roles.guard';
 import { Roles } from '../../core/decorators/roles.decorator';
 import { Role } from '@prisma/client';
 
 @Controller('logs')
-@UseGuards(JwtAuthGuard, RolesGuard)
+@UseGuards(RolesGuard)
 export class LogController {
   constructor(private readonly logService: LogService) {}
 
