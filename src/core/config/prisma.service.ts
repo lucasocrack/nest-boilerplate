f0d1966581c643be82648c0d87ccad633d6d8bd0
Your Name <you@example.com>
2026-01-18 00:02:34 -0300

    feat: Configura o Prisma com um esquema inicial de usuário, log e auditoria, e integra o serviço Prisma ao NestJS.

--- src/core/config/prisma.service.ts
+++ src/core/config/prisma.service.ts
@@ -3,6 +3,15 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      },
+    });
+  }
   async onModuleInit() {
     await this.$connect();
   }
